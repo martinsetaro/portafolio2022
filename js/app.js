5fdb82d91dace3d9cadfb523fbd01218a010db67
martin setaro <lordsesony@gmail.com>
2022-07-06 19:09:03 -0300

    listo para responsive

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
 
-let header = document.getElementById("miHeader");
 
-window.addEventListener('scroll',()=>{
-    let scroll = window.pageYOffset;
-    if(scroll == 0){
-        
-        header.style.background = "transparent";
+const scrollContainer = document.querySelector(".proyectos_contenedor");
 
-        
-    }else{
-        header.style.position = "sticky";
-        header.style.top = "0";
-        header.style.background = "rgba(0,0,0,0.9)";
-    }
-})
+scrollContainer.addEventListener("wheel", (e) => {
+    e.preventDefault();
+    scrollContainer.scrollLeft += e.deltaY;
+}); 
+let link = document.querySelector('.link_arriba')
+
+
+link.onClick = (e)=>{
+    e.preventDefault();
+    window.scroll({
+        top: 0,
+        left: 100,
+        behavior:'smooth',
+      },800)
+
+
+}
 
 
 
